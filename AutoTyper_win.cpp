602e1cd0a53c8c4f2d5ff11112e3b03f54b4321b
Elucidator-xrb <rubixue572@gmail.com>
2025-01-25 17:49:02 +0800

    simple modification, has tested that it can be used

--- AutoTyper_win.cpp
+++ AutoTyper_win.cpp
@@ -1,6 +1,9 @@
 #include <windows.h>
+
 #include <cstdlib>
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -19,7 +22,9 @@ void hide(){
 int key_num[130]={0,0,0,0,0,0,0,0,0,0,13,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,32,1049,11222,1051,1052,1053,1055,10222,11057,1048,1056,1107,188,109,190,111,48,49,50,51,52,53,54,55,56,57,1186,186,11188,187,1190,1191,1050,1065,1066,1067,1068,1069,1070,1071,1072,1073,1074,1075,1076,1077,1078,1079,1080,1081,1082,1083,1084,1085,1086,1087,1088,1089,1090,10219,220,221,1054,1189,192,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,0,1220,1221,1192};
 
 void work(char ch){
-    int num=key_num[ch]%1000,is_shift=key_num[ch]%10000/1000,is_del=key_num[ch]/10000;
+    int num = key_num[ch]%1000;
+    int is_shift = key_num[ch]%10000/1000;
+    int is_del = key_num[ch]/10000;
     if(ch=='{'){
         down(160),an(219),up(160);
         return;
@@ -34,22 +39,31 @@ void str_work(string str){
     for(int i=0;i<str.size();i++) work(char(str[i]));
 }
 
-int main(){
-    hide();
-    while(1) {
-        freopen("target.cpp","r",stdin);
-        while(!key_down(27)) if(key_down(45)&&key_down(8)) return 0;
+int main() {
+    // hide();
+
+    while(true) {
+        // while(!key_down(27)) if(key_down(45)&&key_down(8)) return 0;
+        if (key_down(27)) break;
+
         Sleep(3000);
+        printf("[Start]\n");
+
+        freopen("target.cpp","r",stdin);
+
+        printf("[Typing...]\n");
         char ch;
-        while(ch=getchar()){
-            if(ch==-1) break;
-            if(key_down(45)&&key_down(8)) return 0;
+        while(ch = getchar()){
+            if(ch == -1) break;
+            // if(key_down(45)&&key_down(8)) return 0;
+            if (key_down(27)) break;
             work(ch);
             Sleep(0.5);
         }
         for(int i=1;i<=100;i++) an(46);
-        an(116);
-        str_work("\n\n//Go to https://www.luogu.com.cn/paste/x9pf2cnw to know more!");
+        // an(116);
+
+        break;
     }
     return 0;
 }
--- README.md
+++ README.md
@@ -15,38 +15,47 @@
 **方案一** ：使用现成的xdotool工具
 
 1. 安装xdotool `sudo apt install xdotool`
-2. 执行`autotype.sh`即可（需赋予可执行权限`chmod +d autytype.sh`
+2. 执行 `autotype.sh`即可（需赋予可执行权限 `chmod +d autytype.sh`
 
 存在缺陷
+
 - 由于xdotool似乎不能很好的输出文件中的换行，shell中进行了分行处理
 - shell中对文件内容的操作僵硬，尚不能很好的适配抵消各种编辑器自带的输入优化操作
 
 **方案二** ：C++使用Xlib和XTest库
 
 1. 安装Xlib和XTest， `sudo apt install libx11-dev libxtst-dev`
-2. 执行`make`构建
-3. 运行`./AutoTyper > log.txt` 后，3s内切至目标输入窗口并激活，长按Esc退出
+2. 执行 `make`构建
+3. 运行 `./AutoTyper > log.txt` 后，3s内切至目标输入窗口并激活，长按Esc退出
 
 TODO
+
 - 面向对象化，增加复用和更多模拟
 - CMake构建/跨平台构建
 - 指定输出窗口
 - 增加对编辑器的优化
-  - [x] 抵消换行后的空格自动填充
-  - [x] AC Saber中将4空格视为Tab
+  - [X] 抵消换行后的空格自动填充
+  - [X] AC Saber中将4空格视为Tab
   - [ ] 抵消不同行括号成对匹配
 
-
 #### Wayland服务器
+
 可以用uinput，似乎兼容性更佳（所以应该调查清楚再写）
 
 TODO
 
 ### Windows
-TODO
-姑且来自 https://www.acwing.com/blog/content/44823/ 尚没有测试完善
 
+TODO
+来自 https://www.acwing.com/blog/content/44823/ 
+
+使用Microsoft Visual Studio 提供的编译器
+
+1. 打开Developer Command Prompt for Visual Studio，切换到此目录
+2. `cl AutoTyper_win.cpp /Fe:Autotyper.exe /link User32.lib`进行编译
+3. 随后可运行 `AutoTyper.exe` 
 
 ### Python
+
 TODO
 啧，感觉会更泛用，不过C++写多了一时没意识过来
